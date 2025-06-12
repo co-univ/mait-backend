ff258c4c4fa536aad869b2202250db01986d5aa4
youth <boysoeng@naver.com>
2025-06-12 15:04:52 +0900

    Initial commit

---
+++ src/main/java/com/coniv/mait/MaitApplication.java
@@ -0,0 +1,13 @@
+package com.coniv.mait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/coniv/mait/MaitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coniv.mait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
