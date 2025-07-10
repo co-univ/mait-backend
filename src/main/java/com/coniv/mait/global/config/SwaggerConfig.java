62618b7922c106e29aa377e58d934dae9237ecf9
Youth <109585620+Youthhing@users.noreply.github.com>
2025-07-11 00:43:11 +0900

    [EDU-45] Swagger 설정 (#2)
    * chore: add swagger dependency
    
    * feat: 스웨거 설정

---
+++ src/main/java/com/coniv/mait/global/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.coniv.mait.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.info(new Info()
+				.title("MAIT API Documentation")
+				.description("교육 문제 생성 및 풀이 플랫폼 API")
+				.version("v1.0.0"));
+	}
+} 
