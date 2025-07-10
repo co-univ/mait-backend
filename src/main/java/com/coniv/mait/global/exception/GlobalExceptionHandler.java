3391b4adab31d94a907e0271aaece963198bce5f
youth <boysoeng@naver.com>
2025-07-11 00:59:00 +0900

    feat: method argument validation exception

--- src/main/java/com/coniv/mait/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/coniv/mait/global/exception/GlobalExceptionHandler.java
@@ -1,18 +1,43 @@
 package com.coniv.mait.global.exception;
 
+import java.util.List;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.coniv.mait.global.response.ErrorResponse;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception,
+		HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		ServletWebRequest servletWebRequest = (ServletWebRequest)request;
+		HttpServletRequest httpServletRequest = servletWebRequest.getRequest();
+
+		List<String> messages = exception.getBindingResult().getFieldErrors().stream()
+			.map(DefaultMessageSourceResolvable::getDefaultMessage)
+			.toList();
+
+		log.info("MethodArgumentNotValidException 발생: requestURI={}, error={}", httpServletRequest.getRequestURI(),
+			exception.getMessage());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(ErrorResponse.of(ExceptionCode.USER_INPUT_EXCEPTION, messages));
+	}
 
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ErrorResponse> handleException(Exception exception) {
