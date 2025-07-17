306702d9bd52e2394d0b9f92aa222c7d3d5072b1
gikhoon <gikhoon@gmail.com>
2025-07-17 10:50:03 +0900

    feat: 사용자 DB 셋팅

---
+++ src/main/java/com/coniv/mait/domain/user/entity/UserEntity.java
@@ -0,0 +1,42 @@
+package com.coniv.mait.domain.user.entity;
+
+import com.coniv.mait.domain.user.enums.LoginProvider;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "users")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String email;
+
+	private String password;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Boolean isLocalLogin;
+
+	private String providerId;
+
+	private LoginProvider loginProvider;
+}
