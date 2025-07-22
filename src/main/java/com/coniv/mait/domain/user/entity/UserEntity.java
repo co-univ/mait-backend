2710e5143da94093daf27fabc661122e2b9d96fc
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2025-07-22 09:44:00 +0900

    [EDU-51] 회원 엔티티 설정 (#6)
    * feat: 사용자 DB 셋팅
    
    * feat: 잘못 설정한 어노테이션 수정
    
    * refactor: 로그인 프로바이더 Nullable 설정 변경
    
    * refactor: 개행문자 style 변경
    
    * refactor: checkStyle에 맞게 변경
    
    * feat: providerId에 unique 조건 설정
    
    * feat: email 필드 unique 조건 설정
    
    * feat: 닉네임 필드 추가

---
+++ src/main/java/com/coniv/mait/domain/user/entity/UserEntity.java
@@ -0,0 +1,47 @@
+package com.coniv.mait.domain.user.entity;
+
+import com.coniv.mait.domain.user.enums.LoginProvider;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "users")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true)
+	private String email;
+
+	private String password;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(unique = true)
+	private String nickname;
+
+	private Boolean isLocalLogin;
+
+	@Column(unique = true)
+	private String providerId;
+
+	@Enumerated(EnumType.STRING)
+	private LoginProvider loginProvider;
+
+	//TODO 초대링크 작업 시 팀 필드 추가
+}
---
+++ src/main/java/com/coniv/mait/domain/user/enums/LoginProvider.java
@@ -0,0 +1,13 @@
+package com.coniv.mait.domain.user.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum LoginProvider {
+
+	GOOGLE("구글");
+
+	private final String description;
+}
