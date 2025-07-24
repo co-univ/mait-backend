c6398fa1e365b192945d6474e8cc1bcb449264f1
youth <boysoeng@naver.com>
2025-07-25 00:15:08 +0900

    feat: base question dto 설정

---
+++ src/main/java/com/coniv/mait/domain/question/service/dto/QuestionDto.java
@@ -0,0 +1,25 @@
+package com.coniv.mait.domain.question.service.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public abstract class QuestionDto {
+
+	private Long id;
+
+	private String content;
+
+	private String explanation;
+
+	@NotNull(message = "문제의 번호는 필수입니다.")
+	@Min(value = 1, message = "문제의 번호는 1 이상이어야 합니다.")
+	private Long number;
+}
