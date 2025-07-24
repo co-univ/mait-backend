06195bc4ed6f3b27dfe76ce2bb008dc0083c97a7
youth <boysoeng@naver.com>
2025-07-25 00:13:50 +0900

    feat: 주관식 문제 정답 테이블 설정

---
+++ src/main/java/com/coniv/mait/domain/question/entity/ShortAnswerEntity.java
@@ -0,0 +1,32 @@
+package com.coniv.mait.domain.question.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "short_answers")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ShortAnswerEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String answer;
+
+	@Column(nullable = false)
+	private Long shortAnswerId;
+
+	private boolean isMain;
+
+	@Column(nullable = false)
+	private Long number;
+}
