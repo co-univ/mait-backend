f735d9739a812e648ff21ffb4caaf3201458ac49
youth <boysoeng@naver.com>
2025-07-27 16:35:43 +0900

    feat: 주관식 문제 생성 서비스 메서드

---
+++ src/main/java/com/coniv/mait/domain/question/constant/QuestionType.java
@@ -0,0 +1,8 @@
+package com.coniv.mait.domain.question.constant;
+
+public final class QuestionType {
+	public static final String SHORT = "SHORT";
+	public static final String MULTIPLE = "MULTIPLE";
+	public static final String ORDERING = "ORDERING";
+	public static final String FILL_BLANK = "FILL_BLANK";
+}
