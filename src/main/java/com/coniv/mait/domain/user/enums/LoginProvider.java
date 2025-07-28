2a75cd00024faac94f735ed9d9aad5e3280ea7ca
gikhoon <gikhoon@gmail.com>
2025-07-28 13:24:53 +0900

    refactor: LoginProvider 찾는 방식 변경

--- src/main/java/com/coniv/mait/domain/user/enums/LoginProvider.java
+++ src/main/java/com/coniv/mait/domain/user/enums/LoginProvider.java
@@ -1,6 +1,9 @@
 package com.coniv.mait.domain.user.enums;
 
 import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,11 +17,16 @@ public enum LoginProvider {
 	private final String provider;
 	private final String description;
 
+	private static final Map<String, LoginProvider> PROVIDER_MAP =
+		Arrays.stream(values()).collect(Collectors.toUnmodifiableMap(
+			LoginProvider::getProvider, Function.identity()
+		));
+
 	public static LoginProvider findByProvider(String provider) {
-		return Arrays.stream(values())
-			.filter(lp -> lp.provider.equals(provider))
-			.findFirst()
-			.orElseThrow(() ->
-				new IllegalArgumentException("지원되지 않는 LoginProvider: " + provider));
+		LoginProvider loginProvider = PROVIDER_MAP.get(provider);
+		if (loginProvider == null) {
+			throw new IllegalArgumentException("지원되지 않는 LoginProvider: " + provider);
+		}
+		return loginProvider;
 	}
 }
