c2208b8d9b1179f7771c42f6e46530eb053265cc
youth <boysoeng@naver.com>
2025-07-30 09:03:22 +0900

    feat: 순서 문제 및 보기 테이블 추가

---
+++ src/main/java/com/coniv/mait/domain/question/entity/OrderingOptionEntity.java
@@ -0,0 +1,31 @@
+package com.coniv.mait.domain.question.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "ordering_options")
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrderingOptionEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private int originOrder; // 보기에 보여지는 순서
+
+	private String content;
+
+	private int answerOrder; // 정답이 되는 순서
+}
--- src/main/java/com/coniv/mait/domain/question/entity/OrderingQuestionEntity.java
+++ src/main/java/com/coniv/mait/domain/question/entity/OrderingQuestionEntity.java
@@ -2,9 +2,14 @@ package com.coniv.mait.domain.question.entity;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @DiscriminatorValue("ordering")
+@SuperBuilder
+@AllArgsConstructor(access = lombok.AccessLevel.PROTECTED)
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class OrderingQuestionEntity extends QuestionEntity {
-
 }
