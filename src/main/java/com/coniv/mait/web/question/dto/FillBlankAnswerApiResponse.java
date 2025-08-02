c6d04e6c4ea95c5527eaf847ffe449048236243b
youth <boysoeng@naver.com>
2025-08-03 02:41:40 +0900

    chore: check style and schema description

--- src/main/java/com/coniv/mait/web/question/dto/FillBlankAnswerApiResponse.java
+++ src/main/java/com/coniv/mait/web/question/dto/FillBlankAnswerApiResponse.java
@@ -2,13 +2,20 @@ package com.coniv.mait.web.question.dto;
 
 import com.coniv.mait.domain.question.service.dto.FillBlankAnswerDto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record FillBlankAnswerApiResponse(
+	@Schema(description = "빈칸 문제 답안 ID", requiredMode = Schema.RequiredMode.REQUIRED)
+	Long id,
 	String answer,
+	@Schema(description = "빈칸 문제 답안이 주관식인지 객관식인지 여부", requiredMode = Schema.RequiredMode.REQUIRED)
 	boolean isMain,
+	@Schema(description = "빈칸 문제 답안의 순서", requiredMode = Schema.RequiredMode.REQUIRED)
 	Long number
 ) {
 	public static FillBlankAnswerApiResponse from(FillBlankAnswerDto dto) {
 		return new FillBlankAnswerApiResponse(
+			dto.getId(),
 			dto.getAnswer(),
 			dto.isMain(),
 			dto.getNumber()
