0c55838740b4e1737fb33e71489ef97fea5c9b82
youth <boysoeng@naver.com>
2025-08-04 01:24:05 +0900

    feat: 문제 셋 목록 조회 API

--- src/main/java/com/coniv/mait/web/question/controller/QuestionSetController.java
+++ src/main/java/com/coniv/mait/web/question/controller/QuestionSetController.java
@@ -1,10 +1,14 @@
 package com.coniv.mait.web.question.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.coniv.mait.domain.question.service.QuestionSetService;
@@ -12,6 +16,7 @@ import com.coniv.mait.domain.question.service.dto.QuestionSetDto;
 import com.coniv.mait.global.response.ApiResponse;
 import com.coniv.mait.web.question.dto.CreateQuestionSetApiRequest;
 import com.coniv.mait.web.question.dto.CreateQuestionSetApiResponse;
+import com.coniv.mait.web.question.dto.QuestionSetApiResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -33,5 +38,16 @@ public class QuestionSetController {
 		QuestionSetDto questionSetDto = questionSetService.createQuestionSet(request.subject(), request.creationType());
 		return ResponseEntity.status(HttpStatus.CREATED)
 			.body(ApiResponse.ok(CreateQuestionSetApiResponse.from(questionSetDto)));
+	}
+
+	@Operation(summary = "문제 셋 목록 조회")
+	@GetMapping
+	public ResponseEntity<ApiResponse<List<QuestionSetApiResponse>>> getQuestionSets(
+		@RequestParam("teamId") Long teamId) {
+		List<QuestionSetApiResponse> responses = questionSetService.getQuestionSets(teamId)
+			.stream()
+			.map(QuestionSetApiResponse::from)
+			.toList();
+		return ResponseEntity.ok(ApiResponse.ok(responses));
 	}
 }
