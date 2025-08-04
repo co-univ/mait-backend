f23372969613ba5c17043b3234fad9fa02c5c1cb
youth <boysoeng@naver.com>
2025-08-04 23:51:01 +0900

    feat: 문제 셋에 속한 문제 목록 조회

--- src/main/java/com/coniv/mait/domain/question/repository/QuestionEntityRepository.java
+++ src/main/java/com/coniv/mait/domain/question/repository/QuestionEntityRepository.java
@@ -1,8 +1,11 @@
 package com.coniv.mait.domain.question.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.coniv.mait.domain.question.entity.QuestionEntity;
 
 public interface QuestionEntityRepository extends JpaRepository<QuestionEntity, Long> {
+	List<QuestionEntity> findAllByQuestionSetId(final Long questionSetId);
 }
