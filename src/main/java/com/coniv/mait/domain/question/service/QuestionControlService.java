9a1c35f64ddbdc5bb32aa17ad482a5c55fd3f764
gikhoon <gikhoon@gmail.com>
2025-08-05 14:31:12 +0900

    feat: 퀴즈 엔티티 제어 시 퀴즈가 퀴즈셋에 포함됐는지 확인하는 로직 추가

--- src/main/java/com/coniv/mait/domain/question/service/QuestionControlService.java
+++ src/main/java/com/coniv/mait/domain/question/service/QuestionControlService.java
@@ -3,9 +3,12 @@ package com.coniv.mait.domain.question.service;
 import org.springframework.stereotype.Service;
 
 import com.coniv.mait.domain.question.dto.QuestionStatusMessage;
+import com.coniv.mait.domain.question.entity.QuestionEntity;
 import com.coniv.mait.domain.question.enums.QuestionStatusType;
+import com.coniv.mait.domain.question.repository.QuestionEntityRepository;
 import com.coniv.mait.web.question.controller.QuestionWebSocketController;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -15,30 +18,48 @@ import lombok.extern.slf4j.Slf4j;
 public class QuestionControlService {
 
 	private final QuestionWebSocketController questionWebSocketController;
+	private final QuestionEntityRepository questionEntityRepository;
 
 	/**
 	 * 특정 문제의 접근을 허용
 	 */
-	public void allowQuestionAccess(Long quizSetId, Long questionId) {
+	public void allowQuestionAccess(Long questionSetId, Long questionId) {
+		QuestionEntity question = questionEntityRepository.findById(questionId)
+			.orElseThrow(() -> new EntityNotFoundException("Question not found with id: " + questionId));
+		checkQuestionBelongsToSet(questionSetId, question);
+
 		QuestionStatusMessage message = new QuestionStatusMessage(
-			quizSetId,
+			questionSetId,
 			questionId,
 			QuestionStatusType.ACCESS_PERMISSION
 		);
 
-		questionWebSocketController.broadcastQuestionStatus(quizSetId, message);
+		questionWebSocketController.broadcastQuestionStatus(questionSetId, message);
 	}
 
 	/**
 	 * 특정 문제의 풀이를 허용
 	 */
-	public void allowQuestionSolve(Long quizSetId, Long questionId) {
+	public void allowQuestionSolve(Long questionSetId, Long questionId) {
+		QuestionEntity question = questionEntityRepository.findById(questionId)
+			.orElseThrow(() -> new EntityNotFoundException("Question not found with id: " + questionId));
+		checkQuestionBelongsToSet(questionSetId, question);
+
 		QuestionStatusMessage message = new QuestionStatusMessage(
-			quizSetId,
+			questionSetId,
 			questionId,
 			QuestionStatusType.SOLVE_PERMISSION
 		);
 
-		questionWebSocketController.broadcastQuestionStatus(quizSetId, message);
+		questionWebSocketController.broadcastQuestionStatus(questionSetId, message);
+	}
+
+	private void checkQuestionBelongsToSet(Long questionSetId, QuestionEntity question) {
+		if (!question.getQuestionSet().getId().equals(questionSetId)) {
+			throw new IllegalArgumentException(
+				"Question with id " + question.getQuestionSet().getId() + " does not belong to Set with id "
+					+ questionSetId
+			);
+		}
 	}
 }
