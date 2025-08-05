690219b5ca2d9dac440c89abdcd82b909339da08
youth <boysoeng@naver.com>
2025-08-05 23:59:45 +0900

    feat: 순서 문제 정답 처리 로직 구현

---
+++ src/main/java/com/coniv/mait/domain/solve/service/component/OrderingQuestionAnswerChecker.java
@@ -0,0 +1,39 @@
+package com.coniv.mait.domain.solve.service.component;
+
+import java.util.Comparator;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.coniv.mait.domain.question.entity.OrderingOptionEntity;
+import com.coniv.mait.domain.question.entity.QuestionEntity;
+import com.coniv.mait.domain.question.enums.QuestionType;
+import com.coniv.mait.domain.question.repository.OrderingOptionRepository;
+import com.coniv.mait.domain.solve.service.dto.SubmitAnswerDto;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class OrderingQuestionAnswerChecker implements AnswerChecker<Long> {
+
+	private final OrderingOptionRepository orderingOptionRepository;
+
+	@Override
+	public QuestionType getQuestionType() {
+		return QuestionType.ORDERING;
+	}
+
+	@Override
+	public boolean checkAnswer(QuestionEntity question, SubmitAnswerDto<Long> answers) {
+		List<Long> answerOrders = orderingOptionRepository.findAllByOrderingQuestionId(question.getId()).stream()
+			.sorted(Comparator.comparing(OrderingOptionEntity::getAnswerOrder))
+			.map(OrderingOptionEntity::getOriginOrder)
+			.map(Long::valueOf)
+			.toList();
+
+		List<Long> submitAnswers = answers.getSubmitAnswers();
+
+		return answerOrders.equals(submitAnswers);
+	}
+}
---
+++ src/main/java/com/coniv/mait/domain/solve/service/dto/OrderingQuestionSubmitAnswer.java
@@ -0,0 +1,27 @@
+package com.coniv.mait.domain.solve.service.dto;
+
+import java.util.List;
+
+import com.coniv.mait.domain.question.enums.QuestionType;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class OrderingQuestionSubmitAnswer implements SubmitAnswerDto<Long> {
+
+	@Schema(description = "사용자가 입력한 답변 순서", requiredMode = Schema.RequiredMode.REQUIRED)
+	private List<Long> submitAnswers;
+
+	@Override
+	public QuestionType getType() {
+		return QuestionType.ORDERING;
+	}
+
+	@Override
+	public List<Long> getSubmitAnswers() {
+		return submitAnswers;
+	}
+}
