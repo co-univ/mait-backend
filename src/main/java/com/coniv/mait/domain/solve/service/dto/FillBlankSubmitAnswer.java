0f173799699967680252acd9e299457184284b27
youth <boysoeng@naver.com>
2025-08-06 00:51:29 +0900

    feat: validation 로직 추가

--- src/main/java/com/coniv/mait/domain/solve/service/dto/FillBlankSubmitAnswer.java
+++ src/main/java/com/coniv/mait/domain/solve/service/dto/FillBlankSubmitAnswer.java
@@ -1,7 +1,12 @@
 package com.coniv.mait.domain.solve.service.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record FillBlankSubmitAnswer(
+	@NotNull(message = "빈칸 문제의 번호는 필수입니다.")
 	Long number,
+	@NotBlank(message = "빈칸 문제의 답변은 필수입니다.")
 	String answer
 ) {
 
