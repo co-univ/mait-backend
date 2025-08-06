02fad3c858f48fecc7cf059e070d067798ae5e69
youth <boysoeng@naver.com>
2025-08-07 08:43:21 +0900

    feat: 차단된 토큰 저장소

---
+++ src/main/java/com/coniv/mait/global/jwt/BlackList.java
@@ -0,0 +1,22 @@
+package com.coniv.mait.global.jwt;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@RedisHash(value = "blackList")
+public class BlackList {
+
+	private String id;
+
+	@TimeToLive(unit = TimeUnit.DAYS)
+	@Builder.Default
+	private Long ttl = 7L;
+}
+
