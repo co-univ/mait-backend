0723130d93213abaee9cc1da82d236d0968830cb
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2025-08-07 23:20:37 +0900

    [EDU-116] 유저 정보 반환 API (#59)
    * feat: 유저 정보 반환 API
    
    * style: response 클래스 명 변경

---
+++ src/main/java/com/coniv/mait/web/user/dto/UserInfoApiResponse.java
@@ -0,0 +1,30 @@
+package com.coniv.mait.web.user.dto;
+
+import com.coniv.mait.domain.user.service.dto.UserDto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record UserInfoApiResponse(
+
+	@Schema(description = "사용자 ID")
+	Long id,
+
+	@Schema(description = "사용자 이름")
+	String name,
+
+	@Schema(description = "사용자 이메일")
+	String email,
+
+	@Schema(description = "사용자 닉네임")
+	String nickname
+) {
+
+	public static UserInfoApiResponse from(UserDto userDto) {
+		return new UserInfoApiResponse(
+			userDto.getId(),
+			userDto.getName(),
+			userDto.getEmail(),
+			userDto.getNickname()
+		);
+	}
+}
