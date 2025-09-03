7e0f645f2ea88913fd950b354f7849df35c823e5
Youth <109585620+Youthhing@users.noreply.github.com>
2025-09-03 20:50:59 +0900

    [EDU-110] MDC를 통한 로깅 필터 구현 (#64)
    * feat: random 요청별 id 추가
    
    * feat: 상수 분리
    
    * feat: logback 설정 파일 추가
    
    * feat: 로그백 파일 어펜더 제거
    
    * feat: 현재 사용하지 않는 어펜더 제거 및 패턴 수정

---
+++ src/main/java/com/coniv/mait/global/filter/LoggingFilter.java
@@ -0,0 +1,35 @@
+package com.coniv.mait.global.filter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.slf4j.MDC;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class LoggingFilter extends OncePerRequestFilter {
+
+	private static final String REQUEST_ID = "requestId";
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+
+		// Todo : Request 및 헤더 정보 로깅이 필요하면 현재 위치에 추가
+		String requestId = UUID.randomUUID().toString();
+
+		MDC.put(REQUEST_ID, requestId);
+
+		try {
+			filterChain.doFilter(request, response);
+		} finally {
+			MDC.clear();
+		}
+	}
+}
