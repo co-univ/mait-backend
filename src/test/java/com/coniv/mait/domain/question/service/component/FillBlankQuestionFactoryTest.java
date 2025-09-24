82d503ccaf46d059b665527482d28a0334f571b9
youth <boysoeng@naver.com>
2025-09-25 00:54:28 +0900

    remove unused import line

--- src/test/java/com/coniv/mait/domain/question/service/component/FillBlankQuestionFactoryTest.java
+++ src/test/java/com/coniv/mait/domain/question/service/component/FillBlankQuestionFactoryTest.java
@@ -1,7 +1,6 @@
 package com.coniv.mait.domain.question.service.component;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
