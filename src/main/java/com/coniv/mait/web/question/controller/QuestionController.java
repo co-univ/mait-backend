27b1b7d41c9306704191f7e804eec8d0dcb45928
youth <boysoeng@naver.com>
2025-09-29 22:23:33 +0900

    feat: 기본 문제 생성 API

--- src/main/java/com/coniv/mait/web/question/controller/QuestionController.java
+++ src/main/java/com/coniv/mait/web/question/controller/QuestionController.java
@@ -17,6 +17,7 @@ import com.coniv.mait.domain.question.enums.DeliveryMode;
 import com.coniv.mait.domain.question.enums.QuestionType;
 import com.coniv.mait.domain.question.service.QuestionService;
 import com.coniv.mait.global.response.ApiResponse;
+import com.coniv.mait.web.question.dto.CreateDefaultQuestionApiRequest;
 import com.coniv.mait.web.question.dto.CreateQuestionApiRequest;
 import com.coniv.mait.web.question.dto.QuestionApiResponse;
 import com.coniv.mait.web.question.dto.UpdateQuestionApiRequest;
@@ -44,6 +45,16 @@ public class QuestionController {
 		@PathVariable("questionSetId") final Long questionSetId) {
 		questionService.createQuestion(questionSetId, type, request.toQuestionDto());
 		return ResponseEntity.ok(ApiResponse.noContent());
+	}
+
+	@Operation(summary = "기본 문제 생성 API", description = "문제 생성 과정에서 추가 버튼을 통해 기본 문제를 생성한다.")
+	@PostMapping("/default")
+	public ResponseEntity<ApiResponse<QuestionApiResponse>> createDefaultQuestion(
+		@PathVariable("questionSetId") final Long questionSetId,
+		@RequestBody @Valid CreateDefaultQuestionApiRequest request) {
+		return ResponseEntity.ok(
+			ApiResponse.ok(
+				QuestionApiResponse.from(questionService.createDefaultQuestion(questionSetId, request.number()))));
 	}
 
 	@Operation(summary = "문제 조회 API")
