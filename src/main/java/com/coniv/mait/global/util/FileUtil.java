ab4584f2ab76b6a1c1870e36cacd14033ddfdead
youth <boysoeng@naver.com>
2025-10-02 00:48:07 +0900

    feat: s3 이미지 업로드

---
+++ src/main/java/com/coniv/mait/global/util/FileUtil.java
@@ -0,0 +1,23 @@
+package com.coniv.mait.global.util;
+
+import com.coniv.mait.global.enums.FileExtension;
+import org.springframework.util.StringUtils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class FileUtil {
+
+	public static FileExtension getFileExtension(String fileName) {
+		if (!StringUtils.hasText(fileName) || !fileName.contains(".")) {
+			throw new IllegalArgumentException("Invalid file name: " + fileName);
+		}
+		String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
+		try {
+			return FileExtension.valueOf(extension.toUpperCase());
+		} catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException("Unsupported file extension: " + extension, e);
+		}
+	}
+}
