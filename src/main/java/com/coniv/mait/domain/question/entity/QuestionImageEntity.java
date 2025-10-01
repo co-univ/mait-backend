3c4532f06441d5f01df4e45f7c94c8b7faa36b5c
youth <boysoeng@naver.com>
2025-10-02 01:05:53 +0900

    feat: 이미지 테이블명 변경

--- src/main/java/com/coniv/mait/domain/question/entity/QuestionImageEntity.java
+++ src/main/java/com/coniv/mait/domain/question/entity/QuestionImageEntity.java
@@ -9,12 +9,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+@Table(name = "question_images")
 @Entity
 @Getter
 @SuperBuilder
