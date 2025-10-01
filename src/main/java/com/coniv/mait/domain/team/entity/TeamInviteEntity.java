9e6e02c953c7fe1464ea5f92746cf83f58c05d9d
gikhoon <gikhoon@gmail.com>
2025-10-01 23:31:50 +0900

    feat: 팀 링크 생성 엔티티 구현

---
+++ src/main/java/com/coniv/mait/domain/team/entity/TeamInviteEntity.java
@@ -0,0 +1,48 @@
+package com.coniv.mait.domain.team.entity;
+
+import java.time.LocalDateTime;
+
+import com.coniv.mait.domain.user.entity.UserEntity;
+import com.coniv.mait.global.entity.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "team_invites")
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TeamInviteEntity extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private UserEntity inviter;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "team_id", nullable = false)
+	private TeamEntity team;
+
+	@Column(nullable = false, unique = true)
+	private String token;
+
+	@Column(nullable = false)
+	private LocalDateTime expiresAt;
+}
