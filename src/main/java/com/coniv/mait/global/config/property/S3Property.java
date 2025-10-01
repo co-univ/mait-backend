d9c35f7d0bf9353868aa56cb14578934237494ba
youth <boysoeng@naver.com>
2025-10-02 00:34:13 +0900

    feat: s3 configuration 설정

---
+++ src/main/java/com/coniv/mait/global/config/property/S3Property.java
@@ -0,0 +1,15 @@
+package com.coniv.mait.global.config.property;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties(prefix = "cloud.aws.s3")
+public class S3Property {
+	private String bucket;
+}
