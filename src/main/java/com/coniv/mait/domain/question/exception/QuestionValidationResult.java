a0221180b4c830a34e64adc591c2c3e1d2545b68
youth <boysoeng@naver.com>
2025-10-19 02:21:04 +0900

    feat: question validator case

---
+++ src/main/java/com/coniv/mait/domain/question/exception/QuestionValidationResult.java
@@ -0,0 +1,26 @@
+package com.coniv.mait.domain.question.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum QuestionValidationResult {
+	EMPTY_CONTENT("질문을 입력해주세요."),
+
+	// 객관식
+	INVALID_CHOICE_COUNT("객관식 문제의 선택지는 2개 이상 8개 이하입니다."),
+	MISSING_CHOICE_CONTENT("객관식 문제의 모든 선택지 내용을 입력해주세요."),
+	NO_CORRECT_CHOICE("객관식 문제의 정답이 하나 이상 선택되어야 합니다."),
+
+	// 주관식
+	INVALID_ANSWER_COUNT("주관식 문제의 정답은 1개 이상이어야 합니다."),
+
+	// 빈칸
+	INVALID_BLANK_COUNT("빈칸 문제의 빈칸 개수는 1개 이상이어야 합니다."),
+
+	// 순서
+	INVALID_SEQUENCE_COUNT("순서 문제의 항목은 2개 이상이어야 합니다.");
+
+	private final String description;
+}
