8a59e2c36cc081845ade7a34e20f8255db86cd06
youth <boysoeng@naver.com>
2025-10-19 02:21:23 +0900

    feat: 객관식 문제 검증 로직 추가

---
+++ src/main/java/com/coniv/mait/domain/question/service/dto/QuestionValidateDto.java
@@ -0,0 +1,41 @@
+package com.coniv.mait.domain.question.service.dto;
+
+import com.coniv.mait.domain.question.entity.QuestionEntity;
+import com.coniv.mait.domain.question.exception.QuestionValidationResult;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class QuestionValidateDto {
+
+	private Long questionId;
+
+	private Long number;
+
+	private boolean valid;
+
+	private QuestionValidationResult reason;
+
+	public static QuestionValidateDto invalid(QuestionEntity question, QuestionValidationResult reason) {
+		return QuestionValidateDto.builder()
+			.questionId(question.getId())
+			.number(question.getNumber())
+			.valid(false)
+			.reason(reason)
+			.build();
+	}
+
+	public static QuestionValidateDto valid(QuestionEntity question) {
+		return QuestionValidateDto.builder()
+			.questionId(question.getId())
+			.number(question.getNumber())
+			.valid(true)
+			.build();
+	}
+}
