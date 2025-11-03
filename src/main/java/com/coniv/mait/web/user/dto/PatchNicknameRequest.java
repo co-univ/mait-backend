69577f1005a963700c8317fcb1d69c72e72f10dd
gikhoon <gikhoon@gmail.com>
2025-11-03 22:45:30 +0900

    style: 예외 메세지 수정

--- src/main/java/com/coniv/mait/web/user/dto/PatchNicknameRequest.java
+++ src/main/java/com/coniv/mait/web/user/dto/PatchNicknameRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record PatchNicknameRequest(
-	@NotNull(message = "유지 기간은 필수입니다.")
+	@NotNull(message = "닉네임 값은 필수입니다.")
 	@Size(min = 2, max = 20, message = "닉네임은 2자 이상 20자 이하로 입력해주세요.")
 	String nickname
 ) {
