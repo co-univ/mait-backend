4dfb2bbaee4170e81b5c7ba4431b2e0e8029fc30
gikhoon <gikhoon@gmail.com>
2025-11-05 10:30:49 +0900

    feat: 정책 동의 목록 엔티티 작성

---
+++ src/main/java/com/coniv/mait/domain/user/entity/UserPolicyEntity.java
@@ -0,0 +1,62 @@
+package com.coniv.mait.domain.user.entity;
+
+import java.time.LocalDateTime;
+
+import com.coniv.mait.global.entity.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(
+	uniqueConstraints = {
+		@UniqueConstraint(name = "uk_user_policy_user_policy", columnNames = {"user_id", "policy_id"})
+	},
+	indexes = {
+		@Index(name = "idx_user_policy_user_id", columnList = "user_id"),
+	}
+)
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserPolicyEntity extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private Boolean isChecked;
+
+	@Column(nullable = false)
+	private LocalDateTime checkTime;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private UserEntity user;
+
+	@Column(nullable = false)
+	private Long policyId;
+
+	private UserPolicyEntity(Boolean isChecked, UserEntity user, Long policyId) {
+		this.isChecked = isChecked;
+		this.user = user;
+		this.policyId = policyId;
+		this.checkTime = LocalDateTime.now();
+	}
+
+	public static UserPolicyEntity of(Boolean isChecked, UserEntity user, Long policyId) {
+		return new UserPolicyEntity(isChecked, user, policyId);
+	}
+}
