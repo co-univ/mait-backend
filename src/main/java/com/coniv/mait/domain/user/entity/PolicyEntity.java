1078b65b70f27a1ed83a0ee1bdd4ee8267d31f65
gikhoon <gikhoon@gmail.com>
2025-11-06 14:09:54 +0900

    feat: 정책 동의를 받아야 하는 시점 관련 필드 추가

--- src/main/java/com/coniv/mait/domain/user/entity/PolicyEntity.java
+++ src/main/java/com/coniv/mait/domain/user/entity/PolicyEntity.java
@@ -4,6 +4,7 @@ import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.DynamicInsert;
 
 import com.coniv.mait.domain.user.enums.PolicyCategory;
+import com.coniv.mait.domain.user.enums.PolicyTiming;
 import com.coniv.mait.domain.user.enums.PolicyType;
 import com.coniv.mait.global.entity.BaseTimeEntity;
 
@@ -15,6 +16,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -22,6 +25,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @DynamicInsert
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class PolicyEntity extends BaseTimeEntity {
 
 	@Id
@@ -38,19 +42,17 @@ public class PolicyEntity extends BaseTimeEntity {
 	private PolicyCategory category;
 
 	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private PolicyTiming timing;
+
+	@Column(nullable = false)
 	private String title;
 
-	private PolicyEntity(PolicyCategory category, PolicyType policyType, String title) {
+	@Builder
+	public PolicyEntity(PolicyType policyType, PolicyCategory category, PolicyTiming timing, String title) {
 		this.policyType = policyType;
 		this.category = category;
+		this.timing = timing;
 		this.title = title;
-	}
-
-	public static PolicyEntity essentialPolicy(PolicyCategory category, String title) {
-		return new PolicyEntity(category, PolicyType.ESSENTIAL, title);
-	}
-
-	public static PolicyEntity optionalPolicy(PolicyCategory category, String title) {
-		return new PolicyEntity(category, PolicyType.OPTIONAL, title);
 	}
 }
