7479c7cf30940822d23bea9d3ba68af5211f554b
youth <boysoeng@naver.com>
2025-11-08 16:43:48 +0900

    feat: web client 의존성 추가

---
+++ src/main/java/com/coniv/mait/global/config/WebClientConfig.java
@@ -0,0 +1,29 @@
+package com.coniv.mait.global.config;
+
+import java.time.Duration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import io.netty.channel.ChannelOption;
+import reactor.netty.http.client.HttpClient;
+
+@Configuration
+public class WebClientConfig {
+
+	@Bean
+	public WebClient webClient() {
+		HttpClient httpClient = HttpClient.create()
+			.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+			.responseTimeout(Duration.ofSeconds(10));
+
+		return WebClient.builder()
+			.clientConnector(new ReactorClientHttpConnector(httpClient))
+			.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+			.build();
+	}
+}
