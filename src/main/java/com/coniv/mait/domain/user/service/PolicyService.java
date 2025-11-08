d2e26994d6513e697029c27a53591bcba121d161
gikhoon <gikhoon@gmail.com>
2025-11-08 18:37:58 +0900

    refactor: 엔티티 합침에 따른 서비스 코드 변경

--- src/main/java/com/coniv/mait/domain/user/service/PolicyService.java
+++ src/main/java/com/coniv/mait/domain/user/service/PolicyService.java
@@ -1,19 +1,17 @@
 package com.coniv.mait.domain.user.service;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.coniv.mait.domain.user.entity.PolicyEntity;
-import com.coniv.mait.domain.user.entity.PolicyVersionEntity;
 import com.coniv.mait.domain.user.enums.PolicyTiming;
 import com.coniv.mait.domain.user.repository.PolicyEntityRepository;
-import com.coniv.mait.domain.user.repository.PolicyVersionEntityRepository;
 import com.coniv.mait.domain.user.service.dto.PolicyDto;
 
 import lombok.RequiredArgsConstructor;
@@ -23,7 +21,6 @@ import lombok.RequiredArgsConstructor;
 public class PolicyService {
 
 	private final PolicyEntityRepository policyRepository;
-	private final PolicyVersionEntityRepository policyVersionRepository;
 
 	@Transactional(readOnly = true)
 	public List<PolicyDto> findLatestPolicies(PolicyTiming policyTiming) {
@@ -31,15 +28,17 @@ public class PolicyService {
 		if (policies.isEmpty()) {
 			return List.of();
 		}
-		List<PolicyVersionEntity> latestVersions = policyVersionRepository.findLatestVersionsByPolicies(policies);
 
-		Map<Long, PolicyVersionEntity> latestByPolicyId = latestVersions.stream()
-			.collect(Collectors.toMap(pv -> pv.getPolicy().getId(), Function.identity()));
+		Map<String, List<PolicyEntity>> groupByCode = policies.stream()
+			.collect(Collectors.groupingBy(PolicyEntity::getCode));
 
-		return policies.stream()
-			.map(p -> latestByPolicyId.get(p.getId()))
+		return groupByCode.values().stream()
+			.map(list -> list.stream()
+				.max(Comparator.comparing(PolicyEntity::getVersion))
+				.orElse(null)
+			)
 			.filter(Objects::nonNull)
-			.map(pv -> PolicyDto.from(pv.getPolicy(), pv))
+			.map(PolicyDto::from)
 			.toList();
 	}
 
