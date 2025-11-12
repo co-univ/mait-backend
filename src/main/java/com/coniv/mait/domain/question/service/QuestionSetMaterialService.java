a43cf557d3ced0b0ef32c723c46c5abc04bb6ab3
youth <boysoeng@naver.com>
2025-11-13 00:35:54 +0900

    feat: 최종적으로 저장되는 문제 자료 사용 여부 변경

--- src/main/java/com/coniv/mait/domain/question/service/QuestionSetMaterialService.java
+++ src/main/java/com/coniv/mait/domain/question/service/QuestionSetMaterialService.java
@@ -1,9 +1,13 @@
 package com.coniv.mait.domain.question.service;
 
+import java.util.List;
+
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.coniv.mait.domain.question.dto.MaterialDto;
 import com.coniv.mait.domain.question.entity.QuestionSetMaterialEntity;
 import com.coniv.mait.domain.question.repository.QuestionSetMaterialEntityRepository;
 import com.coniv.mait.domain.question.service.dto.QuestionSetMaterialDto;
@@ -35,5 +39,15 @@ public class QuestionSetMaterialService {
 		);
 
 		return QuestionSetMaterialDto.from(questionSetMaterial);
+	}
+
+	@Async("maitThreadPoolExecutor")
+	@Transactional
+	public void updateUsed(List<MaterialDto> materials) {
+		for (MaterialDto material : materials) {
+			QuestionSetMaterialEntity entity = questionSetMaterialEntityRepository.findById(material.getId())
+				.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 문제 셋 자료입니다. id=" + material.getId()));
+			entity.updateUsage(true);
+		}
 	}
 }
