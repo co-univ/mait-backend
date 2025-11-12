d4013219464a69f28dfa12fda98e2d893ca61bc5
youth <boysoeng@naver.com>
2025-11-13 00:33:35 +0900

    feat: 자료 업로드 시 문제 셋 제거

--- src/main/java/com/coniv/mait/domain/question/service/QuestionSetMaterialService.java
+++ src/main/java/com/coniv/mait/domain/question/service/QuestionSetMaterialService.java
@@ -4,16 +4,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.coniv.mait.domain.question.entity.QuestionSetEntity;
 import com.coniv.mait.domain.question.entity.QuestionSetMaterialEntity;
-import com.coniv.mait.domain.question.repository.QuestionSetEntityRepository;
 import com.coniv.mait.domain.question.repository.QuestionSetMaterialEntityRepository;
 import com.coniv.mait.domain.question.service.dto.QuestionSetMaterialDto;
 import com.coniv.mait.global.component.FileUploader;
 import com.coniv.mait.global.component.dto.FileInfo;
 import com.coniv.mait.global.s3.dto.FileType;
 
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -22,23 +19,18 @@ public class QuestionSetMaterialService {
 
 	private final QuestionSetMaterialEntityRepository questionSetMaterialEntityRepository;
 
-	private final QuestionSetEntityRepository questionSetEntityRepository;
-
 	private final FileUploader fileUploader;
 
 	@Transactional
-	public QuestionSetMaterialDto uploadQuestionSetMaterial(final Long questionSetId, final MultipartFile material) {
-		QuestionSetEntity questionSet = questionSetEntityRepository.findById(questionSetId)
-			.orElseThrow(() -> new EntityNotFoundException("Question Set not found with id: " + questionSetId));
-
+	public QuestionSetMaterialDto uploadQuestionSetMaterial(final MultipartFile material) {
 		FileInfo fileInfo = fileUploader.uploadFile(material, FileType.QUESTION_SET_MATERIAL);
 
 		QuestionSetMaterialEntity questionSetMaterial = questionSetMaterialEntityRepository.save(
 			QuestionSetMaterialEntity.builder()
-				.questionSet(questionSet)
 				.fileKey(fileInfo.getKey())
 				.url(fileInfo.getUrl())
 				.bucket(fileInfo.getBucket())
+				.used(false)
 				.build()
 		);
 
