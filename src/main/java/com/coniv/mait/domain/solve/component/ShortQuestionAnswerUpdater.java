d56d00c04d23aa210f8010891b37fcde8c328544
youth <boysoeng@naver.com>
2025-11-16 15:03:48 +0900

    feat: 주관식 문제에 인정 답안 추가

---
+++ src/main/java/com/coniv/mait/domain/solve/component/ShortQuestionAnswerUpdater.java
@@ -0,0 +1,43 @@
+package com.coniv.mait.domain.solve.component;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.coniv.mait.domain.question.entity.QuestionEntity;
+import com.coniv.mait.domain.question.entity.ShortAnswerEntity;
+import com.coniv.mait.domain.question.enums.QuestionType;
+import com.coniv.mait.domain.question.repository.ShortAnswerEntityRepository;
+import com.coniv.mait.web.solve.dto.ShortUpdateAnswerPayload;
+import com.coniv.mait.web.solve.dto.UpdateAnswerPayload;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class ShortQuestionAnswerUpdater implements QuestionAnswerUpdater {
+
+	private final ShortAnswerEntityRepository shortAnswerEntityRepository;
+
+	@Override
+	public QuestionType getQuestionType() {
+		return QuestionType.SHORT;
+	}
+
+	@Override
+	public void updateAnswer(QuestionEntity question, UpdateAnswerPayload payload) {
+		if (!(payload instanceof ShortUpdateAnswerPayload answers)) {
+			throw new IllegalArgumentException("단답형 문제에만 ShortQuestionAnswerUpdater를 사용할 수 있습니다.");
+		}
+
+		List<ShortAnswerEntity> shortAnswers = answers.shortAnswers().stream()
+			.map(dto -> ShortAnswerEntity.builder()
+				.number(dto.getNumber())
+				.answer(dto.getAnswer())
+				.isMain(dto.isMain())
+				.build())
+			.toList();
+
+		shortAnswerEntityRepository.saveAll(shortAnswers);
+	}
+}
