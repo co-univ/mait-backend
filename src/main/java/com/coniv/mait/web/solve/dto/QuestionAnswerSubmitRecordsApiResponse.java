8fc2b0d28d101d5de49838aae4b49dd039d910d3
youth <boysoeng@naver.com>
2025-11-17 16:31:57 +0900

    feat: 숫자 관련 필드 수정

--- src/main/java/com/coniv/mait/web/solve/dto/QuestionAnswerSubmitRecordsApiResponse.java
+++ src/main/java/com/coniv/mait/web/solve/dto/QuestionAnswerSubmitRecordsApiResponse.java
@@ -10,11 +10,11 @@ public record QuestionAnswerSubmitRecordsApiResponse(
 	@Schema(description = "전체 기록 개수", requiredMode = Schema.RequiredMode.REQUIRED)
 	long totalCounts,
 
-	@Schema(description = "정답 기록 개수", requiredMode = Schema.RequiredMode.REQUIRED)
-	long correctCounts,
+	@Schema(description = "정답자 수", requiredMode = Schema.RequiredMode.REQUIRED)
+	long correctUserCounts,
 
-	@Schema(description = "오답 기록 개수", requiredMode = Schema.RequiredMode.REQUIRED)
-	long incorrectCounts,
+	@Schema(description = "오답자 수", requiredMode = Schema.RequiredMode.REQUIRED)
+	long incorrectUserCounts,
 	@Schema(description = "선착순 제출 기록으로 정렬된 제출 기록 목록", requiredMode = Schema.RequiredMode.REQUIRED)
 	List<QuestionAnswerSubmitRecordApiResponse> submitRecords
 ) {
@@ -22,13 +22,16 @@ public record QuestionAnswerSubmitRecordsApiResponse(
 		List<QuestionAnswerSubmitRecordApiResponse> submitRecords
 	) {
 		long totalCounts = submitRecords.size();
-		long correctCounts = submitRecords.stream().filter(QuestionAnswerSubmitRecordApiResponse::isCorrect).count();
-		long incorrectCounts = totalCounts - correctCounts;
+		long correctUserCounts = submitRecords.stream()
+			.filter(QuestionAnswerSubmitRecordApiResponse::isCorrect)
+			.count();
+		long incorrectUserCounts = submitRecords.stream().filter(record -> !record.isCorrect())
+			.map(QuestionAnswerSubmitRecordApiResponse::userId).distinct().count();
 
 		return new QuestionAnswerSubmitRecordsApiResponse(
 			totalCounts,
-			correctCounts,
-			incorrectCounts,
+			correctUserCounts,
+			incorrectUserCounts,
 			submitRecords
 		);
 	}
