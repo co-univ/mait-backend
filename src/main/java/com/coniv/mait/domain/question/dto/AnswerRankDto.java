0bbea7ef9b73cb67049d4ee8bda5775c488ffa93
youth <boysoeng@naver.com>
2025-11-18 16:38:12 +0900

    feat: 문제 셋 정답자 기준 랭킹 조회 서비스 메서드 구현

---
+++ src/main/java/com/coniv/mait/domain/question/dto/AnswerRankDto.java
@@ -0,0 +1,20 @@
+package com.coniv.mait.domain.question.dto;
+
+import java.util.List;
+
+import com.coniv.mait.domain.user.service.dto.UserDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AnswerRankDto {
+	private long answerCount;
+
+	private List<UserDto> users;
+}
