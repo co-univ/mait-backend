2eb80854956ebd1e880739cd731d7eb2f0ab30a5
gikhoon <gikhoon@gmail.com>
2025-11-21 16:40:35 +0900

    feat: response schema 추가

--- src/main/java/com/coniv/mait/web/team/dto/TeamApiResponse.java
+++ src/main/java/com/coniv/mait/web/team/dto/TeamApiResponse.java
@@ -3,9 +3,16 @@ package com.coniv.mait.web.team.dto;
 import com.coniv.mait.domain.team.enums.TeamUserRole;
 import com.coniv.mait.domain.team.service.dto.TeamUserDto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record TeamApiResponse(
+	@Schema(description = "팀 pk", requiredMode = Schema.RequiredMode.REQUIRED)
 	Long teamId,
+
+	@Schema(description = "팀 이름", requiredMode = Schema.RequiredMode.REQUIRED)
 	String teamName,
+
+	@Schema(description = "팀 내 유저 역할", requiredMode = Schema.RequiredMode.REQUIRED)
 	TeamUserRole role
 ) {
 	public static TeamApiResponse from(TeamUserDto teamUserDto) {
