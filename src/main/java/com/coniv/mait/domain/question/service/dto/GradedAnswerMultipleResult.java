a855045bf00a4184bcccbe856018a64f529ebacf
youth <boysoeng@naver.com>
2026-01-13 17:05:56 +0900

    chore: 스웨거 필드 명세

--- src/main/java/com/coniv/mait/domain/question/service/dto/GradedAnswerMultipleResult.java
+++ src/main/java/com/coniv/mait/domain/question/service/dto/GradedAnswerMultipleResult.java
@@ -1,8 +1,13 @@
 package com.coniv.mait.domain.question.service.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "객관식 문제 채점 결과")
 public record GradedAnswerMultipleResult(
+	@Schema(description = "선택지 번호", example = "1")
 	Long number,
+
+	@Schema(description = "해당 선택지의 정답 여부", example = "true")
 	boolean isCorrect
 ) implements GradedAnswerResult {
 }
-
--- src/main/java/com/coniv/mait/domain/question/service/dto/GradedAnswerResult.java
+++ src/main/java/com/coniv/mait/domain/question/service/dto/GradedAnswerResult.java
@@ -1,6 +1,15 @@
 package com.coniv.mait.domain.question.service.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(
+	description = "채점된 답안 결과",
+	oneOf = {
+		GradedAnswerShortResult.class,
+		GradedAnswerMultipleResult.class,
+		GradedAnswerFillBlankResult.class
+	}
+)
 public interface GradedAnswerResult {
 	boolean isCorrect();
 }
-
