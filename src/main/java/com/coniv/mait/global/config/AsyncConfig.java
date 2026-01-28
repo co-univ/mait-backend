b7f1d5fd80dab0b88f2244df328a79a17d8f7944
youth <boysoeng@naver.com>
2026-01-28 17:43:42 +0900

    [EDU-326] mdc에 저장된 정보 자식 스레드에게 전송

--- src/main/java/com/coniv/mait/global/config/AsyncConfig.java
+++ src/main/java/com/coniv/mait/global/config/AsyncConfig.java
@@ -1,9 +1,12 @@
 package com.coniv.mait.global.config;
 
+import java.util.Map;
 import java.util.concurrent.Executor;
 
+import org.slf4j.MDC;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskDecorator;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
@@ -18,6 +21,7 @@ public class AsyncConfig {
 		taskExecutor.setMaxPoolSize(100);
 		taskExecutor.setQueueCapacity(10000);
 		taskExecutor.setThreadNamePrefix("mait-thread");
+		taskExecutor.setTaskDecorator(new MdcTaskDecorator());
 		taskExecutor.initialize();
 		return taskExecutor;
 	}
@@ -29,7 +33,25 @@ public class AsyncConfig {
 		taskExecutor.setMaxPoolSize(100);
 		taskExecutor.setQueueCapacity(10000);
 		taskExecutor.setThreadNamePrefix("mait-ai");
+		taskExecutor.setTaskDecorator(new MdcTaskDecorator());
 		taskExecutor.initialize();
 		return taskExecutor;
+	}
+
+	public static class MdcTaskDecorator implements TaskDecorator {
+		@Override
+		public Runnable decorate(Runnable runnable) {
+			Map<String, String> contextMap = MDC.getCopyOfContextMap();
+			return () -> {
+				try {
+					if (contextMap != null) {
+						MDC.setContextMap(contextMap);
+					}
+					runnable.run();
+				} finally {
+					MDC.clear();
+				}
+			};
+		}
 	}
 }
