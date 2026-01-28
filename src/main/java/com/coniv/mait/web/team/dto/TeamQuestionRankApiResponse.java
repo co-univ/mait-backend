71414d76d604f8bd3f6229d1951d3841edd34006
gikhoon <gikhoon@gmail.com>
2026-01-28 23:08:34 +0900

    feat: schema 추가

--- src/main/java/com/coniv/mait/web/team/dto/TeamQuestionRankApiResponse.java
+++ src/main/java/com/coniv/mait/web/team/dto/TeamQuestionRankApiResponse.java
@@ -2,10 +2,19 @@ package com.coniv.mait.web.team.dto;
 
 import com.coniv.mait.domain.team.service.dto.TeamQuestionRankDto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record TeamQuestionRankApiResponse(
+	@Schema(description = "유저 ID", requiredMode = Schema.RequiredMode.REQUIRED)
 	Long userId,
+
+	@Schema(description = "유저 이름", requiredMode = Schema.RequiredMode.REQUIRED)
 	String name,
+
+	@Schema(description = "유저 닉네임", requiredMode = Schema.RequiredMode.REQUIRED)
 	String nickname,
+
+	@Schema(description = "점수", requiredMode = Schema.RequiredMode.REQUIRED)
 	Long scorerCount
 ) {
 	public static TeamQuestionRankApiResponse from(TeamQuestionRankDto dto) {
--- src/main/java/com/coniv/mait/web/team/dto/TeamQuestionRankCombinedApiResponse.java
+++ src/main/java/com/coniv/mait/web/team/dto/TeamQuestionRankCombinedApiResponse.java
@@ -4,8 +4,13 @@ import java.util.List;
 
 import com.coniv.mait.domain.team.service.dto.TeamQuestionRankCombinedDto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record TeamQuestionRankCombinedApiResponse(
+	@Schema(description = "선착순 랭킹", requiredMode = Schema.RequiredMode.REQUIRED)
 	List<TeamQuestionRankApiResponse> scorerRank,
+
+	@Schema(description = "정답자 랭킹", requiredMode = Schema.RequiredMode.REQUIRED)
 	List<TeamQuestionRankApiResponse> correctAnswerRank
 ) {
 	public static TeamQuestionRankCombinedApiResponse from(
