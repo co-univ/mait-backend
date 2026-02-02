bc43231e11b39aac1c992c3c8db0cad7337fed2e
youth <boysoeng@naver.com>
2026-02-02 23:58:23 +0900

    feat: 웹소켓 인증 인터셉터 순서 조정

--- src/main/java/com/coniv/mait/global/interceptor/WebSocketAuthInterceptor.java
+++ src/main/java/com/coniv/mait/global/interceptor/WebSocketAuthInterceptor.java
@@ -1,7 +1,9 @@
 package com.coniv.mait.global.interceptor;
 
+import org.springframework.core.annotation.Order;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessagingException;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
@@ -16,6 +18,7 @@ import io.jsonwebtoken.JwtException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@Order(value = 2)
 @Slf4j
 @Component
 @RequiredArgsConstructor
