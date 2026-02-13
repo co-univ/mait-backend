7db42115ed452b1bd60a5b750ef5ae87dde2760e
GiHun Nam <52378919+gikhoon@users.noreply.github.com>
2026-02-13 23:59:37 +0900

    [EDU-312] 팀 문제 랭킹 API 구현 (#196)
    * feat: 선착순 랭크 반환 API 구현
    
    * feat: 정답자 랭크 반환 추가
    
    * refactor: 가독성을 위해 코드 리펙토링
    
    * feat: schema 추가
    
    * feat: param에 맞춰서 수정하도록 변경
    
    * feat: 등수 나오게 변경
    
    * feat: dto 이름 변경
    
    * feat: 랭킹 조회용 dto 변경
    
    * feat: 득점자 및 정답자 조회 api 추가
    
    * feat: 득점자 및 정답자 조회 api 구현
    
    * check style
    
    * feat: api 명세 수정
    
    ---------
    
    Co-authored-by: youth <boysoeng@naver.com>
    (cherry picked from commit b1318019a698a2c52d47080bdc4affc24cefb13b)

---
+++ src/main/java/com/coniv/mait/web/question/controller/TeamQuestionRankController.java
@@ -0,0 +1,59 @@
+package com.coniv.mait.web.question.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.coniv.mait.domain.question.service.TeamQuestionRankService;
+import com.coniv.mait.domain.solve.service.dto.RankDto;
+import com.coniv.mait.domain.user.entity.UserEntity;
+import com.coniv.mait.global.response.ApiResponse;
+import com.coniv.mait.web.question.dto.TeamRankApiResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@Tag(name = "팀 문제 랭킹 API", description = "팀 문제 랭킹 API")
+@RestController
+@RequestMapping("/api/v1/teams/{teamId}/question-ranks")
+@RequiredArgsConstructor
+public class TeamQuestionRankController {
+
+	private final TeamQuestionRankService teamQuestionRankService;
+
+	@Operation(summary = "팀 정답 퀴즈 랭킹 조회", description = "완료된 퀴즈에서 정답자 랭킹 반환",
+		parameters = @Parameter(name = "type", description = "랭킹 타입", required = true, example = "CORRECT"))
+	@GetMapping(params = "type=CORRECT")
+	public ResponseEntity<ApiResponse<TeamRankApiResponse>> getTeamQuestionCorrectRank(
+		@PathVariable("teamId") Long teamId,
+		@Parameter(description = "노출할 랭크 개수")
+		@RequestParam(value = "rankCount", required = false, defaultValue = "5") int rankCount,
+		@AuthenticationPrincipal UserEntity user) {
+		// todo 노출할 rank 조정
+		List<RankDto> ranks = teamQuestionRankService.getTeamQuestionCorrectAnswerRank(teamId);
+		return ResponseEntity.ok(
+			ApiResponse.ok(
+				TeamRankApiResponse.of(ranks, teamId, user.getId(), rankCount, TeamRankApiResponse.RankType.CORRECT)));
+	}
+
+	@Operation(summary = "팀 득점자 퀴즈 랭킹 조회", description = "완료된 퀴즈에서 득점자 랭킹 반환")
+	@GetMapping(params = "type=SCORER")
+	public ResponseEntity<ApiResponse<TeamRankApiResponse>> getTeamQuestionScorerRank(
+		@PathVariable("teamId") Long teamId,
+		@Parameter(description = "노출할 랭크 개수")
+		@RequestParam(value = "rankCount", required = false, defaultValue = "5") int rankCount,
+		@AuthenticationPrincipal UserEntity user) {
+		List<RankDto> ranks = teamQuestionRankService.getTeamQuestionScorerRank(teamId);
+		return ResponseEntity.ok(
+			ApiResponse.ok(
+				TeamRankApiResponse.of(ranks, teamId, user.getId(), rankCount, TeamRankApiResponse.RankType.SCORER)));
+	}
+}
