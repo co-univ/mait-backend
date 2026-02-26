90985641c6d9f57615c25d861dc114f23571cd12
youth <boysoeng@naver.com>
2026-02-27 01:05:06 +0900

    feat: 학습 모드 풀이 시작 API 구현

--- src/main/java/com/coniv/mait/web/solve/controller/StudyModeController.java
+++ src/main/java/com/coniv/mait/web/solve/controller/StudyModeController.java
@@ -2,10 +2,12 @@ package com.coniv.mait.web.solve.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.coniv.mait.domain.solve.service.StudyModeService;
 import com.coniv.mait.global.auth.model.MaitUser;
 import com.coniv.mait.global.response.ApiResponse;
 
@@ -19,9 +21,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class StudyModeController {
 
+	private final StudyModeService studyModeService;
+
 	@Operation(summary = "학습모드 풀이 시작 API", description = "학습 모드 - 문제 풀기 버튼 클릭 시 활용하는 API")
 	@PostMapping
-	public ResponseEntity<ApiResponse<Void>> startStudySubmission(@AuthenticationPrincipal MaitUser user) {
+	public ResponseEntity<ApiResponse<Void>> startStudySubmission(@AuthenticationPrincipal MaitUser user,
+		@PathVariable("questionSetId") Long questionSetId) {
+		studyModeService.startStudyMode(user, questionSetId);
 		return ResponseEntity.ok(ApiResponse.noContent());
 	}
 }
