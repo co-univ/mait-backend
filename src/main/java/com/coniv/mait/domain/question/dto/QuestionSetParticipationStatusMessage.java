0e30c302dbc6716feed21ed138bdc0cac62e54a5
youth <boysoeng@naver.com>
2026-03-31 01:51:27 +0900

    feat: lombok 사용

--- src/main/java/com/coniv/mait/domain/question/dto/QuestionSetParticipationStatusMessage.java
+++ src/main/java/com/coniv/mait/domain/question/dto/QuestionSetParticipationStatusMessage.java
@@ -3,25 +3,19 @@ package com.coniv.mait.domain.question.dto;
 import com.coniv.mait.domain.question.enums.ParticipantStatus;
 import com.coniv.mait.domain.question.enums.QuestionStatusType;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class QuestionSetParticipationStatusMessage {
 
 	private Long questionSetId;
 	private ParticipantStatus participantStatus;
 	private Long questionId;
 	private QuestionStatusType statusType;
-
-	@Builder
-	public QuestionSetParticipationStatusMessage(Long questionSetId, ParticipantStatus participantStatus,
-		Long questionId, QuestionStatusType statusType) {
-		this.questionSetId = questionSetId;
-		this.participantStatus = participantStatus;
-		this.questionId = questionId;
-		this.statusType = statusType;
-	}
 }
