6b96c9e946317ed4bc840c214a85cb9cb8eb7c3e
youth <boysoeng@naver.com>
2026-04-20 23:05:30 +0900

    feat: 삭제되지 않은 팀 조회 메서드 추가

--- src/main/java/com/coniv/mait/domain/team/repository/TeamEntityRepository.java
+++ src/main/java/com/coniv/mait/domain/team/repository/TeamEntityRepository.java
@@ -1,8 +1,11 @@
 package com.coniv.mait.domain.team.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.coniv.mait.domain.team.entity.TeamEntity;
 
 public interface TeamEntityRepository extends JpaRepository<TeamEntity, Long> {
+	Optional<TeamEntity> findByIdAndDeletedAtIsNull(Long id);
 }
