0d4b903c28d4f32b7ef97429597fcb6175e1e68c
Youth <109585620+Youthhing@users.noreply.github.com>
2026-05-07 19:11:16 +0900

    [EDU-494] 팀 문제 셋 카테고리 엔티티 생성 (#280)
    * feat: 카테고리 엔티티 생성
    
    * feat: 카테고리 엔티티 클래스명 변경
    
    * feat: 카테고리 엔티티 디렉토리 변경
    
    * feat: 카테고리 엔티티 테이블명 변경
    
    (cherry picked from commit 69b7be11064b3109605b87211e45fd0c557439e9)

---
+++ src/main/java/com/coniv/mait/domain/question/entity/QuestionSetCategoryEntity.java
@@ -0,0 +1,72 @@
+package com.coniv.mait.domain.question.entity;
+
+import java.time.LocalDateTime;
+
+import com.coniv.mait.global.entity.BaseTimeEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(
+	name = "question_set_categories",
+	uniqueConstraints = {
+		@UniqueConstraint(name = "uk_question_set_categories_teamid_name", columnNames = {"team_id", "name"})
+	},
+	indexes = {
+		@Index(name = "idx_question_set_categories_teamid_deletedat", columnList = "team_id, deleted_at")
+	}
+)
+@Entity
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class QuestionSetCategoryEntity extends BaseTimeEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "team_id", nullable = false)
+	private Long teamId;
+
+	@Column(nullable = false, length = 40)
+	private String name;
+
+	@Column(name = "deleted_at")
+	private LocalDateTime deletedAt;
+
+	private QuestionSetCategoryEntity(final Long teamId, final String name) {
+		this.teamId = teamId;
+		this.name = name;
+	}
+
+	public static QuestionSetCategoryEntity of(final Long teamId, final String name) {
+		return new QuestionSetCategoryEntity(teamId, name);
+	}
+
+	public void updateDeletedAt(final LocalDateTime deletedAt) {
+		this.deletedAt = deletedAt;
+	}
+
+	public void markDeleted() {
+		this.deletedAt = LocalDateTime.now();
+	}
+
+	public void restore() {
+		this.deletedAt = null;
+	}
+
+	public boolean deleted() {
+		return deletedAt != null;
+	}
+}
