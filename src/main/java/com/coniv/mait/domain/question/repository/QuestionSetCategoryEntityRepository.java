a9f4cc1c1a1f3a8a7b92023123f8222b10036d91
youth <boysoeng@naver.com>
2026-05-08 00:46:29 +0900

    feat: 카테고리 목록 조회 도메인 로직 추가
    - Repository: findAllByTeamIdAndDeletedAtIsNullOrderByCreatedAtAsc
    - Service: getCategories - 팀 멤버 권한 검증 후 활성 카테고리 추가순 반환

--- src/main/java/com/coniv/mait/domain/question/repository/QuestionSetCategoryEntityRepository.java
+++ src/main/java/com/coniv/mait/domain/question/repository/QuestionSetCategoryEntityRepository.java
@@ -1,5 +1,7 @@
 package com.coniv.mait.domain.question.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.coniv.mait.domain.question.entity.QuestionSetCategoryEntity;
@@ -9,4 +11,6 @@ public interface QuestionSetCategoryEntityRepository extends JpaRepository<Quest
 	boolean existsByTeamIdAndNameAndDeletedAtIsNull(Long teamId, String name);
 
 	boolean existsByTeamIdAndNameAndDeletedAtIsNotNull(Long teamId, String name);
+
+	List<QuestionSetCategoryEntity> findAllByTeamIdAndDeletedAtIsNullOrderByCreatedAtAsc(Long teamId);
 }
