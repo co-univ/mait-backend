e55365039ebb5c81b187bb2f1626443a4712911f
youth <boysoeng@naver.com>
2026-06-28 11:31:06 +0900

    feat: 온보딩 페이지 코드 추가

--- src/main/java/com/coniv/mait/domain/onboarding/enums/OnboardingScreenCode.java
+++ src/main/java/com/coniv/mait/domain/onboarding/enums/OnboardingScreenCode.java
@@ -1,7 +1,7 @@
 package com.coniv.mait.domain.onboarding.enums;
 
 public enum OnboardingScreenCode {
-
-	QUESTION_SOLVE,   // 문제 풀기 화면
-	QUESTION_MANAGE   // 문제 관리 화면
+	HOME_GUIDE,
+	QUESTION_SOLVE,
+	QUESTION_MANAGE
 }
