526bbd8c2845666aed7f66dddcdd227edd818c32
youth <boysoeng@naver.com>
2026-06-28 11:30:32 +0900

    feat: 온보딩 화면 코드 추가 (문제 풀기/문제 관리)

--- src/main/java/com/coniv/mait/domain/onboarding/enums/OnboardingScreenCode.java
+++ src/main/java/com/coniv/mait/domain/onboarding/enums/OnboardingScreenCode.java
@@ -2,6 +2,6 @@ package com.coniv.mait.domain.onboarding.enums;
 
 public enum OnboardingScreenCode {
 
-	// TODO: 실제 온보딩 화면에 맞게 값 확정
-	HOME_GUIDE
+	QUESTION_SOLVE,   // 문제 풀기 화면
+	QUESTION_MANAGE   // 문제 관리 화면
 }
